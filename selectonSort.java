e5a3d720d9b3edfa6bb47c1c97fd56a138cffd9f
Sahil19560 <110878205+Sahil19560@users.noreply.github.com>
2022-08-17 18:25:15 +0530

    Add files via upload

---
+++ selectonSort.java
@@ -0,0 +1,28 @@
+public class selectonSort {
+    public static void ss(int arr[]){
+        for(int i=0;i<=arr.length-1;i++){
+            int minPos=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[minPos]>arr[j]){
+                    minPos=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[minPos];
+            arr[minPos]=temp;
+
+
+        }
+    }
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String args[]){
+      int arr[]={3,7,5,9,1};
+      ss(arr);
+      printArray(arr);
+    }
+    
+}
