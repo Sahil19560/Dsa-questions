6466c8601dd8093f5c87e35ca14da36643877e73
Sahil19560 <110878205+Sahil19560@users.noreply.github.com>
2022-08-26 11:06:36 +0530

    Add files via upload

--- palindrome.java
+++ palindrome.java
@@ -1,28 +1,18 @@
-import java.util.*;
 public class palindrome {
-    public static boolean isPalindrome(int number){
-        int rev=0;
-        int palindrome=number;
-        while(palindrome!=0){
-            int rem=palindrome%10;
-            rev=rev*10+rem;
-            palindrome/=10;
+    public static boolean isPalindrome(String str){
+        for(int i=0;i<str.length()/2;i++){
+            if(str.charAt(i)!=str.charAt(str.length()-1-i)){
+                return false;
+
+            }
 
         }
-        if(rev==number){
-            return true;
-        }else{
-           return false;
-        }
+        return true;
     }
-        public static void main(String args[]){
-            Scanner sc=new Scanner(System.in);
-            System.out.println("enter number : ");
-            int n=sc.nextInt();
-            if(isPalindrome(n)){
-                System.out.println(n+" is palindrome");
-            }else{
-                System.out.println(n+" is notpalindrome");
-            }
-        }    
+    public static void main(String args[]){
+           String str="sahil";
+           System.out.println(isPalindrome(str));
+           
+
+    }
 }
