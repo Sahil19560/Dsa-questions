996badf053783da2f090ea89137e45fad02df0fb
Sahil19560 <110878205+Sahil19560@users.noreply.github.com>
2022-09-13 18:40:38 +0530

    Add files via upload

---
+++ backtrackingpermutation.java
@@ -0,0 +1,19 @@
+public class backtrackingpermutation {
+    public static void permutation(String str,String ans){
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        for(int i=0;i<str.length();i++){
+            char currChar=str.charAt(i);
+            String newStr=str.substring(0,i)+str.substring(i+1,str.length());
+            permutation(newStr,ans+currChar);
+        }
+    }
+    public static void main(String args[]){
+        String str="abc";
+        permutation(str,"");
+
+    }
+    
+}
